87e3b1cc0dbdff8650587d31229608ce6ef582d9
Joakim Lindquister <joakiml@gmail.com>
2015-01-30 18:20:45 +0100

    Introduced reapting of group elements on the view

--- app/form/form_test.js
+++ app/form/form_test.js
@@ -1,16 +1,24 @@
 'use strict';
 
 describe('app.form module', function() {
+  var formCtrl;
+  var scope;
 
   beforeEach(module('app.form'));
 
   describe('form controller', function(){
+    beforeEach(inject(function($controller, $rootScope){
+      scope = $rootScope.$new();
+      formCtrl = $controller('FormCtrl', {$scope : scope});
+    }));
 
-    it('should ....', inject(function($controller) {
-      //spec body
-      var formCtrl = $controller('FormCtrl');
+    it('should be defined', inject(function() {
       expect(formCtrl).toBeDefined();
     }));
 
+    it('should define groups on the scope', inject( function(){
+      expect(scope.groups).toBeDefined();
+      expect(scope.groups.length).toBeDefined();
+    }));
   });
 });
